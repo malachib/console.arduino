3cd6682481253abc2b77e3be9371334c5df43e99
Malachi Burke <malachi.burke@gmail.com>
2016-03-17 16:54:41 -0700

    Further beefing up menu diagnostics.  Coming up with some theories

--- src/console.cpp
+++ src/console.cpp
@@ -20,6 +20,15 @@ void setBPS2(uint32_t bps)
   monitor.setBPS(bps);
 }
 
+ConsoleService* dummyService;
+
+void menuDiagnostic(int dummyParam)
+{
+  ConsoleService service2;
+  
+  dummyService = &service2;
+}
+
 void gfx_diagnostic_scroll1(int dummyParam);
 void gfx_diagnostic_scroll2();
 
@@ -28,11 +37,31 @@ CREATE_MENUFUNCTION(consoleSetBPS, setBPS, "Change BPS for target device");
 CREATE_MENUFUNCTION(consoleDiagnostic1, gfx_diagnostic_scroll1, "Diagnostic for scroll behavior");
 //CREATE_MENUFUNCTION(consoleDiagnostic2, gfx_diagnostic_scroll2, "Diagnostic for scroll behavior");
 CREATE_MENUFUNCTION(consoleDiagnostic3, setBPS2, "Diagnostic for scroll behavior");
+CREATE_MENUFUNCTION(consoleMenuDiagnostic, menuDiagnostic, "Menu Diagnostic");
 
+// I'm thinking it's possible both static globals are initializing at the same time
+// and next gets assigned to NULL *after* the add statement occurs?
 ConsoleService::MainMenu::MainMenu()
 {
+#ifdef DEBUG
+  cout << F("MainMenu::menus count = ") << count();
+  cout.println();
+#endif
   //add(consoleSetBPS);
+  add(consoleMenuDiagnostic);
+#ifdef DEBUG
+  cout << F("MainMenu::menus count = ") << count();
+  cout.println();
+#endif
   add(consoleDiagnostic1);
+#ifdef DEBUG
+  cout << F("MainMenu::menus count = ") << count();
+  cout.println();
+#endif
   add(consoleDiagnostic3);
+#ifdef DEBUG
+  cout << F("MainMenu::menus count = ") << count();
+  cout.println();
+#endif
 }
 #endif
