5318478da1f73877d56182c43f29462c7f773e9e
Malachi Burke <malachi.burke@gmail.com>
2016-03-17 15:51:43 -0700

    Trying to diagnose missing menu options

--- src/console.cpp
+++ src/console.cpp
@@ -15,6 +15,11 @@ void setBPS(uint32_t bps)
   monitor.setBPS(bps);
 }
 
+void setBPS2(uint32_t bps)
+{
+  monitor.setBPS(bps);
+}
+
 void gfx_diagnostic_scroll1(int dummyParam);
 void gfx_diagnostic_scroll2();
 
@@ -22,10 +27,12 @@ void gfx_diagnostic_scroll2();
 CREATE_MENUFUNCTION(consoleSetBPS, setBPS, "Change BPS for target device");
 CREATE_MENUFUNCTION(consoleDiagnostic1, gfx_diagnostic_scroll1, "Diagnostic for scroll behavior");
 //CREATE_MENUFUNCTION(consoleDiagnostic2, gfx_diagnostic_scroll2, "Diagnostic for scroll behavior");
+CREATE_MENUFUNCTION(consoleDiagnostic3, setBPS2, "Diagnostic for scroll behavior");
 
 ConsoleService::MainMenu::MainMenu()
 {
   add(consoleSetBPS);
   add(consoleDiagnostic1);
+  add(consoleDiagnostic3);
 }
 #endif
