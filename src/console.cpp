1b3e4f8ee0958b958afe4456a106655bbdca31d3
Malachi Burke <malachi.burke@gmail.com>
2016-03-17 17:30:09 -0700

    Cleaning up code since discovering cause of menu glitch.  Needs mild architectural rework (ditch constructor-add approach)

--- src/console.cpp
+++ src/console.cpp
@@ -4,15 +4,19 @@
 #include <fact/MenuFunction.h>
 #include "monitor.h"
 
-ConsoleService console;
-
 using namespace util;
 
 // useful since the touch UI isn't working quite right, and
 // also just for diagnostics
-void setBPS(uint32_t bps)
+int setBPS(uint32_t bps)
 {
   monitor.setBPS(bps);
+  return 1;
+}
+
+int dummyCommand(uint32_t value)
+{
+  return value;
 }
 
 void setBPS2(uint32_t bps)
@@ -22,7 +26,7 @@ void setBPS2(uint32_t bps)
 
 ConsoleService* dummyService;
 
-void menuDiagnostic(int dummyParam)
+void menuDiagnostic()
 {
   ConsoleService service2;
   
@@ -36,32 +40,24 @@ void gfx_diagnostic_scroll2();
 CREATE_MENUFUNCTION(consoleSetBPS, setBPS, "Change BPS for target device");
 CREATE_MENUFUNCTION(consoleDiagnostic1, gfx_diagnostic_scroll1, "Diagnostic for scroll behavior");
 //CREATE_MENUFUNCTION(consoleDiagnostic2, gfx_diagnostic_scroll2, "Diagnostic for scroll behavior");
-CREATE_MENUFUNCTION(consoleDiagnostic3, setBPS2, "Diagnostic for scroll behavior");
+//CREATE_MENUFUNCTION(consoleDiagnostic3, setBPS2, "Diagnostic for scroll behavior");
 CREATE_MENUFUNCTION(consoleMenuDiagnostic, menuDiagnostic, "Menu Diagnostic");
+CREATE_MENUFUNCTION(consoleDummyCommand, dummyCommand, "echoes back input #");
+
+ConsoleService console;
+
 
 // I'm thinking it's possible both static globals are initializing at the same time
 // and next gets assigned to NULL *after* the add statement occurs?
+// NOTE: yes above is true - moving console init below the menuFunctions cleared up
+// the issue.  That is conrete fuel to move away from the constructor-oriented approach
+// and towards a "begin" call instead
 ConsoleService::MainMenu::MainMenu()
 {
-#ifdef DEBUG
-  cout << F("MainMenu::menus count = ") << count();
-  cout.println();
-#endif
-  //add(consoleSetBPS);
+  add(consoleSetBPS);
   add(consoleMenuDiagnostic);
-#ifdef DEBUG
-  cout << F("MainMenu::menus count = ") << count();
-  cout.println();
-#endif
   add(consoleDiagnostic1);
-#ifdef DEBUG
-  cout << F("MainMenu::menus count = ") << count();
-  cout.println();
-#endif
-  add(consoleDiagnostic3);
-#ifdef DEBUG
-  cout << F("MainMenu::menus count = ") << count();
-  cout.println();
-#endif
+  //add(consoleDiagnostic3);
+  add(consoleDummyCommand);
 }
 #endif
